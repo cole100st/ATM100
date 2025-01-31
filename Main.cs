811df9f66dcc82f93ba01fc95343fcb5f815a75e
cole100st <walid755937@gmail.com>
2025-01-31 10:58:52 +0100

    Main.cs

---
+++ Main.cs
@@ -0,0 +1,21 @@
+using Life;
+using UnityEngine;
+using Life.UI;
+using Life.Network;
+using ModKit.Helper;
+using System.Collections.Generic;
+using System;
+using ModKit.Interfaces;
+using System.Reflection;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Main
+{
+    public class PlayerData
+    {
+        public int PlayerId { get; set; }
+        public int PinCode { get; set; }
+}   }
