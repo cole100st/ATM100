e2bed033b6c27d8bb4e7053171dcdd6c84ce88eb
cole100st <walid755937@gmail.com>
2025-01-31 10:59:42 +0100

    Main.cs

--- Main.cs
+++ Main.cs
@@ -18,4 +18,24 @@ namespace Main
     {
         public int PlayerId { get; set; }
         public int PinCode { get; set; }
-}   }
+    }
+
+    public class PlayerDatabase
+    {
+        private readonly string dbPath = "playerData.json";
+        private List<PlayerData> players;
+
+        public PlayerDatabase()
+        {
+            if (File.Exists(dbPath))
+            {
+                var json = File.ReadAllText(dbPath);
+                players = string.IsNullOrWhiteSpace(json)
+                    ? new List<PlayerData>()
+                    : JsonConvert.DeserializeObject<List<PlayerData>>(json) ?? new List<PlayerData>();
+            }
+            else
+            {
+                players = new List<PlayerData>();
+            }
+        }
